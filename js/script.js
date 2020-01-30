dfbdce7aac61c747407614c4f756ab62f8d2de8f
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-30 16:55:38 +0100

    Finished main exercise

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 
-  $.ajax( {
-    url: "https://flynn.boolean.careers/exercises/api/random/int",
-    method: "GET",
-    success: function (data, state) {
-      console.log(data, state);
-    },
-    error: function (request, state, error) {
-      console.log(state, error);
-    } }
-  );
+  $(document).on('click', '.box', function() {
+
+    var selection = $(this);
+
+    $.ajax( {
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (data, state) {
+        console.log(data);
+        var number = data.response;
+        if (number <= 5) {
+          selection.addClass('yellow');
+        } else {
+          selection.addClass('green');
+        }
+      },
+      error: function (request, state, error) {
+        console.log(state, error);
+      } }
+    );
+
+  });
 
 });
